82b830c17d1f33cc18fbe3467201dece61adb856
Ramesh S <rameshsudhan11@gmail.com>
2025-02-22 08:16:02 +0530

    Pubsub

--- main.tf
+++ main.tf
@@ -26,46 +26,46 @@ resource "google_pubsub_subscription" "sub2" {
   }
 }
 
-# resource "google_pubsub_subscription" "sub3" {
-#   name = "test-3-sub"
-#   project = var.project_id
-#   topic = google_pubsub_topic.topic1.name
-#   ack_deadline_seconds = 10
-#   message_retention_duration = "86400s"
-#   expiration_policy {
-#     ttl = ""
-#   }
-# }
+resource "google_pubsub_subscription" "sub3" {
+  name = "test-3-sub"
+  project = var.project_id
+  topic = google_pubsub_topic.topic1.name
+  ack_deadline_seconds = 10
+  message_retention_duration = "86400s"
+  expiration_policy {
+    ttl = ""
+  }
+}
 
-# resource "google_pubsub_subscription" "sub4" {
-#   name = "test-4-sub"
-#   project = var.project_id
-#   topic = google_pubsub_topic.topic1.name
-#   ack_deadline_seconds = 10
-#   message_retention_duration = "86400s"
-#   expiration_policy {
-#     ttl = ""
-#   }
-# }
+resource "google_pubsub_subscription" "sub4" {
+  name = "test-4-sub"
+  project = var.project_id
+  topic = google_pubsub_topic.topic1.name
+  ack_deadline_seconds = 10
+  message_retention_duration = "86400s"
+  expiration_policy {
+    ttl = ""
+  }
+}
 
-# resource "google_pubsub_subscription" "sub5" {
-#   name = "test-5-sub"
-#   project = var.project_id
-#   topic = google_pubsub_topic.topic1.name
-#   ack_deadline_seconds = 10
-#   message_retention_duration = "86400s"
-#   expiration_policy {
-#     ttl = ""
-#   }
-# }
+resource "google_pubsub_subscription" "sub5" {
+  name = "test-5-sub"
+  project = var.project_id
+  topic = google_pubsub_topic.topic1.name
+  ack_deadline_seconds = 10
+  message_retention_duration = "86400s"
+  expiration_policy {
+    ttl = ""
+  }
+}
 
-# resource "google_pubsub_subscription" "sub6" {
-#   name = "test-6-sub"
-#   project = var.project_id
-#   topic = google_pubsub_topic.topic1.name
-#   ack_deadline_seconds = 10
-#   message_retention_duration = "86400s"
-#   expiration_policy {
-#     ttl = ""
-#   }
-# }
+resource "google_pubsub_subscription" "sub6" {
+  name = "test-6-sub"
+  project = var.project_id
+  topic = google_pubsub_topic.topic1.name
+  ack_deadline_seconds = 10
+  message_retention_duration = "86400s"
+  expiration_policy {
+    ttl = ""
+  }
+}
