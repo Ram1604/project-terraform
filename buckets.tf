a0833ad8f54f956ac73c95221e4f55b1be85d9b9
Ramesh S <rameshsudhan11@gmail.com>
2025-03-08 12:50:56 +0530

    test

--- buckets.tf
+++ buckets.tf
@@ -11,7 +11,7 @@ resource "google_tags_location_tag_binding" "bucket_tag" {
 }
 
 resource "google_storage_bucket" "bucket_2" {
-  name = "test-bucket12"
+  name = "test-bucket12-xvs"
   project = var.project_id
   location = "us-central1"
 }
