bfe6b55fe168c5fb56ab042790621d6b66ab6606
Ramesh S <rameshsudhan11@gmail.com>
2025-03-08 11:33:40 +0530

    test

--- buckets.tf
+++ buckets.tf
@@ -4,7 +4,7 @@ resource "google_storage_bucket" "bucket_1" {
   location = "us-central1"
 }
 
-resource "google_tags_tag_binding" "bucket_tag" {
+resource "google_tags_location_tag_binding" "bucket_tag" {
   parent    = google_storage_bucket.bucket_1.id
   tag_value = "tagValues/281481500673264"
 }
